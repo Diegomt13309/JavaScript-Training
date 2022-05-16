c65ec18128d3e238f41be7af00391f4e1b2ae24b
Diego Monterrey <demilio12@hotmail.com>
2022-05-16 16:30:27 -0600

    Operators

--- 01-Fundamentals-Part-1/starter/assingments.js
+++ 01-Fundamentals-Part-1/starter/assingments.js
@@ -1,5 +1,17 @@
-//#1
+//#1 Values and Variables
 let country = 'Costa Rica';
 let continent = 'America';
 let population = 7000000;
 console.log(`My country ${country}, is located in the continent of ${continent} and has about ${population} people living there.!`);
+//#2 Data Types
+let isIsland = false;
+let language;
+console.log(isIsland);
+console.log(language);
+console.log(`Type Of some variables: {isIsland : ${typeof isIsland}}, {population: ${typeof population}}, {country: ${typeof country}} and {language : ${typeof language}}.`);
+//#3 Basic Operators
+const halfPopulation = population / 2;
+population++;
+console.log(`Population divided by half: ${halfPopulation}, increasing the country population by 1: ${population}, Filand has 6 million people, it has more than my country? ${6000000 > population}. The average population of a country is 33M, my country have less than 33M? ${population < 33000000}.`);
+const description = 'Portugal is in Europe, and its 11 Million people speak portuguese';
+console.log(description);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -28,5 +29,81 @@ let myCurrentJob = 'QA';
 let job1 = 'Programmer';
 let job2 = 'QA';
 //]
-
 console.log(myFirstJob);
+*/
+//************************************************** */
+/*Data Types
+true;
+console.log(true);
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Diego');
+//Dynamic typing
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+//Undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+//Error
+console.log(typeof null);*/
+/* *************************************************
+//let, const and var
+//let declare variables that can change later, mutable
+let age = 30;
+age = 31;
+//const does not change, inmutable
+const birthYear = 1997;
+// birthYear = 1998;
+
+//illegal, needs an initialization
+//const job;
+
+//Its a good practice to use const
+
+//var should be avoided - Legacy
+var job = 'programmer';
+job = 'techer';
+
+//do not use this, bad practice
+// lastName = 'Monterrey';
+// console.log(lastName);
+*/
+/*************************************************** */
+//Operators
+//Math operators
+const now = 2037;
+const ageDiego = now - 1997;
+const ageSara = now - 2020;
+console.log(ageDiego, ageSara);
+
+console.log(ageDiego * 2, ageDiego / 10, 2 ** 3);
+// 2 ** 3, means 2 to the power of 3 == 2*2*2
+
+const firstName = 'Diego';
+const lastName = 'Monterrey';
+console.log(firstName + ' ' + lastName);
+//Assigment Operators
+let x = 10 + 5; //x = 10
+x += 10; // x=x+10; x = 25;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+//Comparison operators
+console.log(ageDiego > ageSara); //> < <= >=
+console.log(ageSara >= 18);
+
+const isFullAge = ageSara >= 18;
+
+console.log(now - 1991 > now - 2018);
