9c025d6006eba38de1b8fd832c1f9bbabce7ec52
Diego Monterrey <demilio12@hotmail.com>
2022-05-15 22:02:12 -0600

    Second

---
+++ 01-Fundamentals-Part-1/starter/assingments.js
@@ -0,0 +1,5 @@
+//#1
+let country = 'Costa Rica';
+let continent = 'America';
+let population = 7000000;
+console.log(`My country ${country}, is located in the continent of ${continent} and has about ${population} people living there.!`);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+//values, piece of data - fundamental unit
+console.log('Jonas');
+console.log(23);
+//declaring a variable
+let firstName = 'Diego';
+let first = 'Jonas';
+let firstNamePerson = 'Jhon';
+let first_name = 'Kari';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+//illegal
+//let 4year = 9; number before any name
+//let jonas&matilda = 'jm'; &
+//let new = 2; reserve key words
+//name is legal but sometimes is not
+//let Per = 'Diego'; no illegal, but is not recommended
+let PI = 3.1415;
+
+//[
+//this is better, very descriptive than
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'QA';
+//this, this is not descriptive
+let job1 = 'Programmer';
+let job2 = 'QA';
+//]
+
+console.log(myFirstJob);
